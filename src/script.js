43b16ebb3474b8d648dd6224602309886adbbd2f
Rupesh Garhwal <rupeshgarhwal3920@gmail.com>
2021-08-10 01:43:40 +0530

    navbar changes

--- src/script.js
+++ src/script.js
@@ -2,11 +2,15 @@ $(document).ready(function () {
 	console.log("hello");
 	$(window).scroll(function () {
 		let scroll = $(window).scrollTop();
-
 		if (scroll > 300) {
 			$(".navbar").addClass("nav-bg");
 		} else {
 			$(".navbar").removeClass("nav-bg");
 		}
 	});
+
+	console.log($(window).width());
+	if ($(window).width() < 992) {
+		$(".navbar").removeClass("fixed-top");
+	}
 });
